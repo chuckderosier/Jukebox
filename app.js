7e55782ba8b36b306df3fcf15ca75a625e23d4a0
chuckderosier <chuckderosier2018@gmail.com>
2018-10-25 15:26:36 -0400

    finished boilerplate

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,6 +7,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI)
 
 var app = express();
 
