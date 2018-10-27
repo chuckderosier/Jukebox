3f91c10a1b2c6782087d9cf72c920a7b1bae697f
chuckderosier <chuckderosier2018@gmail.com>
2018-10-27 17:58:51 -0400

    album routes

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,13 @@ router.post('/addArtist', artistController.create)
 router.get('/artist/:id/editArtist', nameController.edit)
 router.patch('/artist/:id', artistController.update)
 router.delete('/artist/:id', artistController.delete)
-
+// album routes
+router.get('/', albumController.index)
+router.get('/album/:id', albumController.show)
+router.get('/addAlbum', albumController.new)
+router.post('/addAlbum', albumController.create)
+router.get('/album/:id/editAlbum', nameController.edit)
+router.patch('/album/:id', albumController.update)
+router.delete('/album/:id', albumController.delete)
 
 module.exports = router
