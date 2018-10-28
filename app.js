7734b8b9904304735726b58132e3c84a7daa5fd2
chuckderosier <chuckderosier2018@gmail.com>
2018-10-28 13:34:03 -0400

    fixed a bug

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI)
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
